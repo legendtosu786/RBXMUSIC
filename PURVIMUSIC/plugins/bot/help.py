c88292f826e6c8b9a1af393a72ebef126ff8e52c
ppppprivate <xdd28209@gmail.com>
2025-04-24 17:24:31 +0530

    Update help.py

--- PURVIMUSIC/plugins/bot/help.py
+++ PURVIMUSIC/plugins/bot/help.py
@@ -14,7 +14,7 @@ from PURVIMUSIC.misc import SUDOERS
 from PURVIMUSIC.utils.stuffs.buttons import BUTTONS
 from PURVIMUSIC.utils.stuffs.helper import Helper
 
-@app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
+@app.on_message(filters.command(["mhelp"]) & filters.private & ~BANNED_USERS)
 @app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
 async def helper_private(
     client: app, update: Union[types.Message, types.CallbackQuery]
@@ -47,7 +47,7 @@ async def helper_private(
         )
 
 
-@app.on_message(filters.command(["help"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["mhelp"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def help_com_group(client, message: Message, _):
     keyboard = private_help_panel(_)
