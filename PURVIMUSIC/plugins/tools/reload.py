52e1159076a834f86d4ed7071c8e3c0abf55f730
ppppprivate <xdd28209@gmail.com>
2025-04-24 17:26:14 +0530

    Update reload.py

--- PURVIMUSIC/plugins/tools/reload.py
+++ PURVIMUSIC/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(7403621976)
+    & filters.user(7685111725)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
