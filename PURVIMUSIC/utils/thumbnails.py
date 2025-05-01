fe0e18eb4db62362575f1c0c18f319c7706355e4
ppppprivate <xdd28209@gmail.com>
2025-05-01 17:27:51 +0530

    Add files via upload

--- PURVIMUSIC/utils/thumbnails.py
+++ PURVIMUSIC/utils/thumbnails.py
@@ -1,11 +1,9 @@
 import os
 import re
-import textwrap
-import random
 
 import aiofiles
 import aiohttp
-from PIL import Image, ImageDraw, ImageEnhance, ImageOps, ImageFilter, ImageFont
+from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 
@@ -29,10 +27,6 @@ def clear(text):
         if len(title) + len(i) < 60:
             title += " " + i
     return title.strip()
-
-
-def get_random_color():
-    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), 255)
 
 
 async def get_thumb(videoid):
@@ -71,86 +65,48 @@ async def get_thumb(videoid):
                     await f.close()
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"Spy/assets/dil.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
-        background = image2.filter(filter=ImageFilter.BoxBlur(9))
+        background = image2.filter(filter=ImageFilter.BoxBlur(10))
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(0.5)
-
-        image3 = changeImageSize(1280, 720, bg)
-        image5 = image3.convert("RGBA")
-        Image.alpha_composite(background, image5).save(f"cache/temp{videoid}.png")
-
-        Xcenter = youtube.width / 2
-        Ycenter = youtube.height / 2
-        x1 = Xcenter - 250
-        y1 = Ycenter - 250
-        x2 = Xcenter + 250
-        y2 = Ycenter + 250
-
-        logo = youtube.crop((x1, y1, x2, y2))
-        logo.thumbnail((360, 360), Image.Resampling.LANCZOS)
-
-        border_size = 13
-        border_color = get_random_color()
-
-        bordered_logo = Image.new("RGBA", (logo.width + 2 * border_size, logo.height + 2 * border_size), (0, 0, 0, 0))
-        bordered_logo.paste(logo, (border_size, border_size))
-
-        draw = ImageDraw.Draw(bordered_logo)
-        draw.rectangle(
-            [(0, 0), (bordered_logo.width - 1, bordered_logo.height - 1)],
-            outline=border_color,
-            width=border_size
-        )
-
-        background.paste(bordered_logo, (750, 160), bordered_logo)
-        background.paste(image3, (0, 0), mask=image3)
-
         draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("Spy/assets/font2.ttf", 45)
-        font2 = ImageFont.truetype("Spy/assets/font2.ttf", 70)
-        arial = ImageFont.truetype("Spy/assets/font2.ttf", 30)
-        name_font = ImageFont.truetype("Spy/assets/font.ttf", 30)
-        para = textwrap.wrap(title, width=30)
-        j = 0
-        draw.text((5, 5), f"Alisamusic", fill="white", font=name_font)
-        for line in para:
-            if j == 1:
-                j += 1
-                draw.text(
-                    (60, 260),
-                    f"{line}",
-                    fill="white",
-                    stroke_width=1,
-                    stroke_fill="white",
-                    font=font,
-                )
-            if j == 0:
-                j += 1
-                draw.text(
-                    (60, 210),
-                    f"{line}",
-                    fill="white",
-                    stroke_width=1,
-                    stroke_fill="white",
-                    font=font,
-                )
+        arial = ImageFont.truetype("PURVIMUSIC/assets/font2.ttf", 30)
+        font = ImageFont.truetype("PURVIMUSIC/assets/font.ttf", 30)
+        text_size = draw.textsize("TEAM PURVI BOTS    ", font=font)
+        draw.text((1280 - text_size[0] - 10, 10), "TEAM THUNDER BOTS    ", fill="white", font=font)
         draw.text(
-            (20, 675),
+            (55, 560),
             f"{channel} | {views[:23]}",
             (255, 255, 255),
             font=arial,
         )
         draw.text(
-            (60, 400),
+            (57, 600),
+            clear(title),
+            (255, 255, 255),
+            font=font,
+        )
+        draw.line(
+            [(55, 660), (1220, 660)],
+            fill="white",
+            width=5,
+            joint="curve",
+        )
+        draw.ellipse(
+            [(918, 648), (942, 672)],
+            outline="white",
+            fill="white",
+            width=15,
+        )
+        draw.text(
+            (36, 685),
             "00:00",
             (255, 255, 255),
             font=arial,
         )
         draw.text(
-            (610, 400),
+            (1185, 685),
             f"{duration[:23]}",
             (255, 255, 255),
             font=arial,
